0e526abf434b4c3ea9713b92836e54477905fc93
Hanna Schmueck <67383627+hannaschmueck@users.noreply.github.com>
2026-08-31 14:56:08 +0200

    cleaner zip handling for HistDiscMod Project

--- puima/inception_zip_file_util.py
+++ puima/inception_zip_file_util.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('../')
-
 """
 Puima: a lightweight Python-framework to process the text part of UIMA CAS structures.
 
@@ -10,6 +7,7 @@ You can run the script on files extracted using the "UIMA XML 1.1" format.
 """
 
 __author__ = "Annemarie Friedrich"
+__editor__ = "Hanna Schmueck"
 __license__ = "Apache 2.0"
 __version__ = "1.0"
 
@@ -17,16 +15,18 @@ from zipfile import ZipFile, is_zipfile
 import os, shutil, sys
 from puima.fix_xmi import fix_typenames_etc
 
-def unzip_archive(top_path, in_path, out_path, annotator_of_interest, curation):
+def unzip_archive(top_path, in_path, out_path, zip_name, annotator_of_interest, annotator_outpath, curation):
     """
+    :param top_path: top-level path
     :param in_path: path to zip archive (INCEpTION export)
+    :param zip_name: filename of the zip archive
     :param out_path: where to write the unzipped files
     :param annotator_of_interest: only the annotations of this annotator are kept!
     :return:
     """
-    in_path = os.path.join(top_path, in_path)
+    in_path = os.path.join(top_path, in_path, zip_name)
     out_path = os.path.join(top_path, out_path)
-    xmi_out_path = os.path.join(top_path, "temp_xmi_docs")  # path where to write the relevant XMI files
+    xmi_out_path = os.path.join(top_path, annotator_outpath)  # path where to write the relevant XMI files
 
     if os.path.exists(xmi_out_path):
         shutil.rmtree(xmi_out_path)
@@ -36,35 +36,56 @@ def unzip_archive(top_path, in_path, out_path, annotator_of_interest, curation):
 
     # unzip first level
     with ZipFile(in_path, 'r') as zf:
-        # zf.printdir()
         print('Extracting all the files now...')
         zf.extractall(out_path)
         print('Done!')
+
+
 
     annotations_folder = "annotation"
     if curation == "true":
         annotations_folder = "curation"
 
     # unzip inner levels
-    for folder in os.listdir(os.path.join(out_path, annotations_folder)):
-        print(folder)
-        for zipfilename in os.listdir(os.path.join(out_path, annotations_folder, folder)):
-            if not is_zipfile(os.path.join(out_path, annotations_folder, folder, zipfilename)):
-                continue
-            print("\t", zipfilename)
-            zf = ZipFile(os.path.join(out_path, annotations_folder, folder, zipfilename), 'r')
-            zf.extractall(os.path.join(out_path, annotations_folder, folder))
+    try:
+        for folder in os.listdir(os.path.join(out_path, annotations_folder)):
+            print(folder)
+            for zipfilename in os.listdir(os.path.join(out_path, annotations_folder, folder)):
+                if not is_zipfile(os.path.join(out_path, annotations_folder, folder, zipfilename)):
+                    continue
+                print("\t", zipfilename)
+                zf = ZipFile(os.path.join(out_path, annotations_folder, folder, zipfilename), 'r')
+                zf.extractall(os.path.join(out_path, annotations_folder, folder))
 
-            # copy file to xmi_docs folder, changing the filename
-            shutil.copy(os.path.join(out_path, annotations_folder, folder, "TypeSystem.xml"),
-                            os.path.join(xmi_out_path))
-            if os.path.exists(os.path.join(out_path, annotations_folder, folder, annotator_of_interest + ".xmi")):
-                target_loc = folder
-                if not target_loc.endswith(".xmi"):
-                    target_loc += ".xmi"
-                shutil.copy(os.path.join(out_path, annotations_folder, folder, annotator_of_interest + ".xmi"),
-                            os.path.join(xmi_out_path, target_loc))
+                # copy file to xmi_docs folder, changing the filename
+                shutil.copy(os.path.join(out_path, annotations_folder, folder, "TypeSystem.xml"),
+                                os.path.join(xmi_out_path))
+                if os.path.exists(os.path.join(out_path, annotations_folder, folder, annotator_of_interest + ".xmi")):
+                    target_loc = folder
+                    if not target_loc.endswith(".xmi"):
+                        target_loc += ".xmi"
+                    shutil.copy(os.path.join(out_path, annotations_folder, folder, annotator_of_interest + ".xmi"),
+                                os.path.join(xmi_out_path, target_loc))
+    except FileNotFoundError as fnf:
+        out_path = os.path.join(out_path, zip_name[:-4])
+        for folder in os.listdir(os.path.join(out_path, annotations_folder)):
+            print(folder)
+            for zipfilename in os.listdir(os.path.join(out_path, annotations_folder, folder)):
+                if not is_zipfile(os.path.join(out_path, annotations_folder, folder, zipfilename)):
+                    continue
+                print("\t", zipfilename)
+                zf = ZipFile(os.path.join(out_path, annotations_folder, folder, zipfilename), 'r')
+                zf.extractall(os.path.join(out_path, annotations_folder, folder))
 
+                # copy file to xmi_docs folder, changing the filename
+                shutil.copy(os.path.join(out_path, annotations_folder, folder, "TypeSystem.xml"),
+                                os.path.join(xmi_out_path))
+                if os.path.exists(os.path.join(out_path, annotations_folder, folder, annotator_of_interest + ".xmi")):
+                    target_loc = folder
+                    if not target_loc.endswith(".xmi"):
+                        target_loc += ".xmi"
+                    shutil.copy(os.path.join(out_path, annotations_folder, folder, annotator_of_interest + ".xmi"),
+                                os.path.join(xmi_out_path, target_loc))
     # count number of XMIs found
     count = 0
     for filename in os.listdir(xmi_out_path):
@@ -78,9 +99,10 @@ if __name__ == '__main__':
     base_path = sys.argv[1]  # the base folder where we are operating, INCEpTION export as zip should be in this folder
     source_path = sys.argv[2] # Zip file is the export as UIMA XMI 1.1 from Inception
     target_path = sys.argv[3] # path for extracted zip files
-    userid = sys.argv[4]  # the Inception userid of the user whose annotations are to be extracted as XMI documents
-    final_target_path = sys.argv[5]  # folder with XMI documents with fixed document name attributes in XMI
-    curation = sys.argv[6]
+    zip_name = sys.argv[4]
+    userid = sys.argv[5]  # the Inception userid of the user whose annotations are to be extracted as XMI documents
+    annotator_outpath = sys.argv[6]
+    final_target_path = sys.argv[7]  # folder with XMI documents with fixed document name attributes in XMI
     if curation not in ["true", "false"]:
         print("curation needs to be true or false, if false: extrating from annotations folder.")
     unzip_archive(base_path, source_path, target_path, userid, curation)
@@ -88,12 +110,11 @@ if __name__ == '__main__':
         shutil.rmtree(os.path.join(base_path, final_target_path))
     
     os.makedirs(os.path.join(base_path, final_target_path))
-    fix_typenames_etc(os.path.join(base_path, "temp_xmi_docs"), os.path.join(base_path, final_target_path))
+    fix_typenames_etc(os.path.join(base_path, annotator_outpath), os.path.join(base_path, final_target_path))
 
-    if os.path.exists(os.path.join(base_path, "temp_xmi_docs")):
-        shutil.rmtree(os.path.join(base_path, "temp_xmi_docs"))
+    if os.path.exists(os.path.join(base_path, annotator_outpath)):
+        shutil.rmtree(os.path.join(base_path, annotator_outpath))
 
     # Further example usage:
     # This script puts the unzipped file into "extracted_file"
-    # unzip_archive(os.path.join("../data", "project-name"), "export.zip",
-    #              "extracted_file", "nt_user")
+    # unzip_archive(top_path="", in_path="data", zip_name="sample_project.zip", out_path="data/output", annotator_of_interest="annotator.id", curation=False)
